c84f1db784a5330252a1200be3e92d352c23b8d8
Alex Wong <wongalexjx@gmail.com>
2022-03-27 02:24:03 -0700

    added comments

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ import re
 
 
 def parse_this():
-    with open("input.txt", "r") as f, open("new_file.txt", "w") as n:
-        for line in f:
+    """
+    removes all chars except a-z!
+    :return: text file.
+    """
+    with open("input.txt", "r") as file, open("new_file.txt", "w") as new:
+        for line in file:
             line.strip()
             result = re.sub("[^a-z\s]", "", line, 0, re.IGNORECASE | re.MULTILINE)
-            n.write(result)
+            new.write(result)
 
 
 def add_text():
+    """
+    add additional text needed.
+    :return: text file.
+    """
     with open("new_file1.txt", "r") as file, open("new_file2.txt", "w") as new:
         for line in file:
             new.write(f"<item>{line}<\item>\n")
@@ -17,4 +25,5 @@ def add_text():
 
 if __name__ == '__main__':
     parse_this()
+    # manual data fixing required? pycharm uses TAB + SHIFT to undent and TAB to indent
     add_text()
